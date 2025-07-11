73048f5116a4c5d1dc06c040e71936e119c68736
ShahrinTarin <rahmanshahrintarin@gmail.com>
2025-07-11 19:57:32 +0600

    first commit

---
+++ src/Routes/Router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router";
+import Home from "../Pages/Home";
+import RootLayOut from "../LayOuts/RootLayOut";
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    Component: RootLayOut,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        index: true,
+        hydrateFallbackElement: <Loader></Loader>,
+        Component: Home,
+
+      },
+       ]
+
+  }
+])
+
+export default router
+    
