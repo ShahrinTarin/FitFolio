9c7aacdeef0e8569bd03d0a1182bc61a53095c9e
ShahrinTarin <rahmanshahrintarin@gmail.com>
2025-08-16 15:15:50 +0600

    done

--- src/components/Dashboard/Sidebar/Menu/AdminMenu.jsx
+++ src/components/Dashboard/Sidebar/Menu/AdminMenu.jsx
@@ -3,10 +3,10 @@ import MenuItem from './MenuItem'
 const AdminMenu = () => {
   return (
     <>
+      <MenuItem icon={FaWallet} label='Overview' address='/dashboard/balance' />
       <MenuItem icon={FaEnvelopeOpenText} label='All Newsletter' address='/dashboard/newsletter' />
       <MenuItem icon={FaUserTie } label='All Trainers' address='/dashboard/trainers' />
       <MenuItem icon={FaClipboardCheck} label='Applied Trainer' address='/dashboard/applied-trainers' />
-      <MenuItem icon={FaWallet} label='Balance' address='/dashboard/balance' />
       <MenuItem icon={FaChalkboardTeacher} label='Add new Class' address='/dashboard/add-class' />
     </>
   )
